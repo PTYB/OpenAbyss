dc87087481eb0cdd187db8471a4f01a1a1482214
PTYB <80309041+PTYB@users.noreply.github.com>
2022-02-05 17:30:35 +1300

    Logging tidyup

--- src/com/open/abyss/leaf/CrossDitch.kt
+++ src/com/open/abyss/leaf/CrossDitch.kt
@@ -15,8 +15,6 @@ import org.powbot.api.script.tree.Leaf
 import java.util.logging.Logger
 
 class CrossDitch(script: Script) : Leaf<Script>(script, "Crossing ditch") {
-
-    private var logger: Logger = Logger.getLogger(this.javaClass.simpleName)
     private val ditchTile: Tile get() = script.configuration.teleport.ditchTile
 
     override fun execute() {
@@ -25,7 +23,6 @@ class CrossDitch(script: Script) : Leaf<Script>(script, "Crossing ditch") {
             .shuffled().firstOrNull() ?: GameObject.Nil
 
         if (ditch == GameObject.Nil) {
-            logger.info("Invalid GO walking to ${ditch.tile}")
             Movement.builder(ditchTile.derive(0, Random.nextInt(0, -1)))
                 .setWalkUntil { ditchTile.distance() < 3 }
                 .setRunMin(50)
@@ -38,12 +35,11 @@ class CrossDitch(script: Script) : Leaf<Script>(script, "Crossing ditch") {
 
     private fun walkToDitch(ditch: GameObject) {
         if (!ditch.inViewport() || ditch.tile.distance() >= 3) {
-            logger.info("Walking to ditch viewport ${ditch.inViewport()},  dist ${ditch.tile.distance()} ${ditch.tile}")
             val targetTile = Tile(ditch.tile.x, 3520)
             LocalPathFinder.findPath(targetTile)
                 .traverseUntilReached(3.0)
         }
-        logger.info("Walking to ditch viewport ${ditch.inViewport()}, and attempting interacting")
+
         if (ditch.inViewport() && ditch.interact("Cross")) {
             Condition.wait({ Players.local().y() >= 3523 }, 1000, 10)
         }
